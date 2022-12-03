230cacba040785c968ff1824bcbbc09666131ffa
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-12-03 21:58:52 +0000

    config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    minHeight: {
+      50: "50px",
+    },
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
