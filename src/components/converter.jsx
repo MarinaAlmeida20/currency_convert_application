cc223aa1201ee322fbe4578f2dd31226d526534a
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-12-03 22:57:32 +0000

    form

--- src/components/converter.jsx
+++ src/components/converter.jsx
@@ -6,6 +6,20 @@ function converterNames() {
       <h1 className="text-black text-2xl mb-10 font-semibold">
         Currency Exchange Rate
       </h1>
+
+      <form>
+        <label className="font-bold text-sm mb-3 block" htmlFor="text">
+          Amount
+        </label>
+        <input
+          className=" focus:outline-none focus:right-1 focus:ring-green-500 focus:border-green-500 
+       border-2 rounded-sm w-full py-2 pl-3 pr-10"
+          type="text"
+          value=""
+          size="lg"
+          placeholder="Enter amount"
+        />
+      </form>
     </section>
   );
 }
