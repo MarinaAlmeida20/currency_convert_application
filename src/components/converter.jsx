e1b155af52736294a73d29eaa84bfd3f1b59d59a
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-12-03 23:01:07 +0000

    form

--- src/components/converter.jsx
+++ src/components/converter.jsx
@@ -8,17 +8,60 @@ function converterNames() {
       </h1>
 
       <form>
-        <label className="font-bold text-sm mb-3 block" htmlFor="text">
-          Amount
-        </label>
-        <input
-          className=" focus:outline-none focus:right-1 focus:ring-green-500 focus:border-green-500 
+        <div className="flex flex-row mb-6 gap-9 items-center">
+          <div className="flex-1">
+            <label className="font-bold text-sm mb-3 block" htmlFor="text">
+              Amount
+            </label>
+            <input
+              className=" focus:outline-none focus:right-1 focus:ring-green-500 focus:border-green-500 
        border-2 rounded-sm w-full py-2 pl-3 pr-10"
-          type="text"
-          value=""
-          size="lg"
-          placeholder="Enter amount"
-        />
+              type="text"
+              value=""
+              size="lg"
+              placeholder="Enter amount"
+            />
+          </div>
+          <div className="flex-1">
+            <label className="font-bold text-sm mb-3 block" htmlFor="text">
+              Amount
+            </label>
+            <input
+              className=" focus:outline-none focus:right-1 focus:ring-green-500 focus:border-green-500 
+       border-2 rounded-sm w-full py-2 pl-3 pr-10"
+              type="text"
+              value=""
+              size="lg"
+              placeholder="Enter amount"
+            />
+          </div>
+          <div className="flex-1">
+            <label className="font-bold text-sm mb-3 block" htmlFor="text">
+              Amount
+            </label>
+            <input
+              className=" focus:outline-none focus:right-1 focus:ring-green-500 focus:border-green-500 
+       border-2 rounded-sm w-full py-2 pl-3 pr-10"
+              type="text"
+              value=""
+              size="lg"
+              placeholder="Enter amount"
+            />
+          </div>
+          <div className="flex-1">
+            <label className="font-bold text-sm mb-3 block" htmlFor="text">
+              Amount
+            </label>
+            <input
+              className=" focus:outline-none focus:right-1 focus:ring-green-500 focus:border-green-500 
+       border-2 rounded-sm w-full py-2 pl-3 pr-10"
+              type="text"
+              value=""
+              size="lg"
+              placeholder="Enter amount"
+            />
+          </div>
+        </div>
       </form>
     </section>
   );
